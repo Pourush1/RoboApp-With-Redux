83f2f00865e525eb1d11e89974920d519d2f0251
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2019-04-17 15:04:01 -0400

    Added some styling to the App using App.css and SEGA.Woff LogoFont file, also set the background using linear gradient which is not working currently and also added className to the h1

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "./CardList";
 import { robots } from "./robots";
 import SearchBox from "./SearchBox";
+import "./App.css";
 
 //Defined a state to communicate the robots from the CardList and the SearchField
 //Get used to this syntax
@@ -28,7 +29,7 @@ class App extends Component {
     });
     return (
       <div className="tc">
-        <h1>Robo Friends</h1>
+        <h1 className="f1">Robo Friends</h1>
         <SearchBox searchChange={this.onSearchChange} />
         <CardList robots={filteredRobots} />
       </div>

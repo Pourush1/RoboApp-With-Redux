402ff8c8d14f690e03c83422c07ec02a8cba30e7
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2019-08-28 14:43:43 -0500

    Added the Redux middleware to keep track of the state, logs of the application usung redux-looger and creteLogger

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import { createLogger } from "redux-logger";
 
 import "./index.css";
 import App from "./containers/App";
@@ -10,8 +11,11 @@ import { robots } from "./robots";
 import "tachyons";
 import { searchRobots } from "./reducers";
 
+//logger function which is a middlware we can apply that to our redux app, and this can be done using something called applyMiddleware which comes from redux package
+const logger = createLogger();
+
 //store uses the root reducer or whatever reducer we have to create a store and create that object tree of our state
-const store = createStore(searchRobots);
+const store = createStore(searchRobots, applyMiddleware(logger));
 
 //PRovider component passes down the store
 ReactDOM.render(
--- src/reducers.js
+++ src/reducers.js
@@ -7,7 +7,7 @@ const initialState = {
 //need to create a reducer, also giving a default state and action inside the bracket
 export const searchRobots = (state = initialState, action = {}) => {
   //reducer runs on every action and says did CHANGESEARCHFIELD actions happen if that's the case I am goona return a new state
-  console.log(action.type);
+
   switch (action.type) {
     case CHANGE_SEARCH_FIELD:
       return Object.assign({}, state, { searchField: action.payload });

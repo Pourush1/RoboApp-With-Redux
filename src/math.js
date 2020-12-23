5f37ce7599655ee91859c07121c940d7b10ba6ce
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2020-12-23 01:44:43 -0800

    removing the test portion

--- src/math.js
+++ src/math.js
@@ -3,8 +3,6 @@ export const Sub = (a, b) => a - b;
 export const Mul = (a, b) => a * b;
 export const Div = (a, b) => a / b;
 
-console.log('a')
-
 export const Total = (subTotal, tax) => {
   return `$ ${Sum(subTotal, tax)}`;
 };

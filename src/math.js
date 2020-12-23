1033b36497def895fc80e887508282707638d926
Pourush1 <pourush.shrestha@sagarmatha.edu.np>
2020-12-23 01:43:58 -0800

    testing some git

--- src/math.js
+++ src/math.js
@@ -3,6 +3,8 @@ export const Sub = (a, b) => a - b;
 export const Mul = (a, b) => a * b;
 export const Div = (a, b) => a / b;
 
+console.log('a')
+
 export const Total = (subTotal, tax) => {
   return `$ ${Sum(subTotal, tax)}`;
 };
